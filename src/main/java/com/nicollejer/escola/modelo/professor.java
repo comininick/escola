f0593da161a28514a763ecbfdbfc8001583386ec
comininick <nicolle.jerke@escola.pr.gov.br>
2025-06-27 11:40:18 -0300

    classe professor

---
+++ src/main/java/com/nicollejer/escola/modelo/professor.java
@@ -0,0 +1,75 @@
+package com.nicollejer.escola.modelo;
+
+public class professor {
+    
+
+    private String nome;
+    private String cpf;
+
+    @Deprecated
+    public professor (){
+    }
+
+    public professor(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public String toString() {
+        return "professor [nome=" + nome + ", cpf=" + cpf + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        professor other = (professor) obj;
+        if (cpf == null) {
+            if (other.cpf != null)
+                return false;
+        } else if (!cpf.equals(other.cpf))
+            return false;
+        return true;
+    }
+
+    
+    
+
+    
+
+
+
+
+
+
+
+}
