5335fb5e30012c07adc9da2163d4007eeba5f562
comininick <nicolle.jerke@escola.pr.gov.br>
2025-07-31 08:38:10 -0300

    foi

---
+++ src/main/java/com/nicollejer/escola/controller/ProfessorController.java
@@ -0,0 +1,111 @@
+package com.nicollejer.escola.controller;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import javax.swing.Spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.nicollejer.escola.dto.ProfessorDto;
+import com.nicollejer.escola.modelo.Professor;
+import com.nicollejer.escola.repository.ProfessorRepository;
+
+
+@RestController //transforma a nossa classe em um controller antigamente chamado de bean
+@RequestMapping(value = "/professor") // mapeando a url, navegador chama pelo vetor
+public class ProfessorController {
+
+    @Autowired
+    private ProfessorRepository professorRepository;    
+
+    @GetMapping(value = "/imprimir") //mapeando do metodo imprimir. usando o verbo get -> pegar ou buscar
+    public void imprimir(){//void -> não retorna nada
+        System.out.println("chamou o metodo imprimir");
+    }
+
+    @GetMapping(value = "/ola")
+    public String ola(){  //addicionado retorno como String -> tipo: texto //nao quer dizer html
+        return "<h1>Nicolle linda</h1>"; //return: devolve a resposta pra quem chamou// nao funciona obv com "alert"
+    }
+
+    @GetMapping( value = "/listaprofessor")
+    public List<Professor> findAll(){
+        return professorRepository.findAll();
+    }
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        professorRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+     @GetMapping( value = "/{id}")
+    public ResponseEntity<Professor> ProfessorDoBanco (@PathVariable Long id){
+        Optional<Professor> ProfessorDoBanco = professorRepository.findById(id);
+
+          return ResponseEntity.ok(ProfessorDoBanco.get());
+    } 
+
+    @GetMapping( value = "/{nome}")
+    public ResponseEntity<Professor> consultarporNome(@PathVariable String nome){
+        Professor professorBuscado = professorRepository.findByIdNome(nome);
+         
+         
+        return ResponseEntity.ok(professorBuscado);
+    }
+
+
+             
+            
+             @PostMapping( value = "/insert")
+    public ResponseEntity<?> insert(@RequestBody ProfessorDto professorDto){
+
+        Professor professor = professorDto.novoProfessor();
+
+        professorRepository.save(professor);
+
+        System.out.println(professor.toString());
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                                            .path("/id")
+                                            .buildAndExpand(professor.getId())
+                                            .toUri();
+
+        return ResponseEntity.created(uri).body(professor);        
+    }
+
+}
+
+// teste commit nicolle porra
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/nicollejer/escola/repository/ProfessorRepository.java
@@ -0,0 +1,11 @@
+package com.nicollejer.escola.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.nicollejer.escola.modelo.Professor;
+
+public interface ProfessorRepository extends JpaRepository<Professor, Long>{
+    @Query (value = "select * from professor where nome like ?", nativeQuery = true)
+    public Professor findByIdNome(String nome);
+}
